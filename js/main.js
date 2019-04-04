553d9a479b91fa218b91a2bc08748efdb99c95ab
Marko Jordanovic <mrakaconi@gmail.com>
2019-04-04 21:51:39 +0200

    pocetak

---
+++ js/main.js
@@ -0,0 +1,21 @@
+fetch('https://randomuser.me/api/?results=20')
+    .then(response => response.json())
+    .then(podaci => prikaziPodatke(podaci))
+
+
+function prikaziPodatke(korisnici) {
+
+    for (let i = 0; i < korisnici.results.length; i++) {
+        const user = korisnici.results[i];
+
+        var $div = $("<div class=student></div>");
+
+        $div.append("<h1>" + user.name.first + "</h1>"),
+        $div.append("<h1>" + user.name.last + "</h1>"),
+        $div.append("<p>" + user.email + "</p>"),
+        $div.append("<p>" + user.phone + "</p>"),
+        $div.append(`<img src="${user.picture.medium}">`),
+
+        $("#okvir").append($div);
+    }
+}  
