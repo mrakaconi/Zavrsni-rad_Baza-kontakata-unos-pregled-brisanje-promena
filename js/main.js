4ae81fd4a8636617ea68de6a18da34830f14fae4
mrakaconi <47000661+mrakaconi@users.noreply.github.com>
2019-04-10 14:41:59 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@
     for (let i = 0; i < results.length; i++) {
         const user = results[i];
 
-        var $okvir = $("<div class=contacts></div>");
+        var $div = $("<div class=contacts></div>");
 
-        $okvir.append(`<img src="${user.picture.large}">`);
-        $okvir.append("<h1>" + user.name.first + "</h1>"),
-            $okvir.append("<h1>" + user.name.last + "</h1>"),
-            $okvir.append("<p>" + user.email + "</p>"),
+        $div.append(`<img src="${user.picture.large}">`);
+        $div.append("<h1>" + user.name.first + "</h1>"),
+            $div.append("<h1>" + user.name.last + "</h1>"),
+            $div.append("<p>" + user.email + "</p>"),
 
-            $("#okvir").append($okvir);
+            $("#okvir").append($div);
     }
 };
 
