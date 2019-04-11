f472c548f3ea4e76b3575216791f902204e5d810
Marko Jordanovic <mrakaconi@gmail.com>
2019-04-11 14:12:58 +0200

    izrada_7

--- js/main.js
+++ js/main.js
@@ -1,20 +1,25 @@
 for (let i = 0; i < results.length; i++) {
-        const user = results[i];
-
-
+        let user = results[i];
+        
         var $div = $("<div class=contacts></div>");
-
 
         $div.append(`<img src="${user.picture.large}">`);
         $div.append("<h1>" + user.name.first + "</h1>"),
-                $div.append("<h1>" + user.name.last + "</h1>"),
-                $div.append("<p>" + user.email + "</p>"),
+        $div.append("<h1>" + user.name.last + "</h1>"),
+        $div.append("<p>" + user.email + "</p>"),
 
-                $("#okvir").append($div);
+        $("#okvir").append($div);
 };
 
+// pretraga kontakata unutar "grid" pregleda
+
+
+
+
+// Pravljenje dinamicke tabele sa preuzimanjem podataka i popunjavanjem celija
+
 function drawTable(tbody) {
-        var user = results;
+        let user = results;
         var tr, td;
         tbody = document.getElementById(tbody);
         table = document.getElementById("tabela");
@@ -39,3 +44,5 @@ function drawTable(tbody) {
         }
 }
 drawTable("podaci");
+
+
